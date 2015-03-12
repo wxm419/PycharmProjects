75b5bea6da5d313b610cf9fd301c9a04257369b6
猥琐的胖子 <wxm419@gmail.com>
2015-03-12 18:38:48 +0800

    修复中文无法显示的问题

--- Ubox/ubox_monitor.py
+++ Ubox/ubox_monitor.py
@@ -16,14 +16,18 @@ while isLoop:
     result = json.loads(response.text)
     if result["head"]["status"] == 1:
         print datetime.datetime.today()
-        print result["data"]["name"], u"产品列表"
+        print result["data"]["name"]
         for each in result["data"]["Product"]:
             print each["shortName"], each["num"]
             products[each["shortName"]] = each["num"]
         if len(last_products) > 0:
+            fresh_meat = []
             for key in products:
-                if last_products.get(key, 0) == 0 and products.get(key, 0) > 0:
-                    ctypes.windll.user32.MessageBoxA(0, '%s 上货啦！' % key, u'行动吧吃货！', 0)
-                    isLoop = False
+                if last_products.get(key, 0) > 0 and products.get(key, 0) > 0:
+                    fresh_meat.append(key)
+            if len(fresh_meat) > 0:
+                result = reduce(lambda x, y: x + ", " + y, fresh_meat, "").strip(", ")
+                ctypes.windll.user32.MessageBoxA(0, (u'%s 上货啦！' % result).encode("gbk"), u'行动吧吃货！'.encode("gbk"), 0)
+                isLoop = False
         last_products = products.copy()
     time.sleep(60)
